621559ae478fdc8059ba18f8bcefe75925323b47
Arauko <Arauko@192.168.1.6>
2017-04-19 20:41:32 +0100

    test

---
+++ FormatText/src/patdis/es/FomatText/FormatoTexto.java
@@ -0,0 +1,17 @@
+package patdis.es.FomatText;
+
+/**
+ * Hello world!
+ *
+ */
+public class FormatoTexto{
+	
+	public FormatoTexto() {
+		super();
+	}
+	
+	public void adapterPrint(Object txtInic, String msg){
+		System.out.println(txtInic + " : " + msg);
+	}
+	
+}
